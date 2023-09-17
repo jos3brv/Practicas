057e05d9f94df0aed28d45372e5365a3bfbdbb94
jos3brv <jos3brv@gmail.com>
2023-09-16 19:39:52 -0600

    Ejercicio 2 completado

--- p04/p03_funciones.php
+++ p04/p03_funciones.php
@@ -23,6 +23,60 @@
         }
     ?>
 
+    <h2>Ejercicio 2</h2>
+    <?php
+    
+    function generarImpar() {
+        return rand(1, 100) * 2 - 1;
+    }
+
+    
+    function generarPar() {
+        return rand(1, 100) * 2;
+    }
+
+    
+    $matriz = array();
+    $iteraciones = 0;
+    $numerosGenerados = 0;
+
+    
+    while (true) {
+        $numero1 = generarImpar();
+        $numero2 = generarPar();
+        $numero3 = generarImpar();
+        
+       
+        if ($numero1 % 2 == 1 && $numero2 % 2 == 0 && $numero3 % 2 == 1) {
+           
+            $matriz[] = array($numero1, $numero2, $numero3);
+            $iteraciones++;
+            $numerosGenerados += 3;
+            
+           
+            if ($iteraciones >= 4) {
+                break;
+            }
+        }
+    }
+
+    
+    echo "<p>Matriz: </p>";
+    echo "<table border='1'>";
+    foreach ($matriz as $fila) {
+        echo "<tr>";
+        foreach ($fila as $numero) {
+            echo "<td>$numero</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+
+    
+    echo "<p>Número de iteraciones: $iteraciones</p>";
+    echo "<p>Cantidad de números generados: $numerosGenerados</p>";
+    ?>
+
     <h2>Ejemplo de POST</h2>
     <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
         Name: <input type="text" name="name"><br>
