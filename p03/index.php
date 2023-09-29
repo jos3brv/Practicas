98a00d82653096a3acbaf04df2280d2f0245e97b
jos3brv <jos3brv@gmail.com>
2023-09-28 22:26:38 -0600

    Ejercicio 1 completado

--- p03/index.php
+++ p03/index.php
@@ -5,26 +5,70 @@
     <title>Práctica 3</title>
 </head>
 <body>
+     <p>
+    <a href="http://validator.w3.org/check?uri=referer"><img
+      src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+  </p>
     <h2>Ejercicio 1</h2>
     <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
     <p>$_myvar,  $_7var,  myvar,  $myvar,  $var7,  $_element1, $house*5</p>
-    <?php
-        //AQUI VA MI CÓDIGO PHP
-        $_myvar;
-        $_7var;
-        //myvar;       // Inválida
-        $myvar;
-        $var7;
-        $_element1;
-        //$house*5;     // Invalida
+    <ul><li>$_myvar es válida porque inicia con guión bajo.</li><li>$_7var es válida porque inicia con guión bajo.</li><li>$myvar es válida porque inicia con una letra.</li><li>$var7 es válida porque inicia con una letra.</li><li>$_element1 es válida porque inicia con guión bajo.</li></ul>
+    <h2>Ejercicio 2</h2>
+    <h4>a)</h4>
+<p>ManejadorSQL</p>
+<p>MySQL</p>
+<p>ManejadorSQL</p>
+<h4>c)</h4>
+<p>PHP server</p>
+<p>PHP server</p>
+<p>PHP server</p>
+<h4>a)</h4>
+<p>ManejadorSQL</p>
+<p>MySQL</p>
+<p>ManejadorSQL</p>
+<h4>d)</h4>
+<p>PHP server</p>
+<p>PHP server</p>
+<p>PHP server</p>
+<p>En el segundo bloque de asignaciones, se cambió el valor de la variable $a de "ManejadorSQL" a "PHP server" mediante la asignación $a = "PHP server". Luego, la variable $b se convirtió en una referencia a $a mediante la asignación $b = $a. Esto significa que tanto $a como $b apuntan a la misma ubicación de memoria y, por lo tanto, contienen el mismo valor "PHP server". Además, la variable $c, que ya era una referencia a $a desde el primer conjunto de asignaciones, también refleja el cambio y contiene "PHP server".</p>
+    <h2>Ejercicio 3</h2>
+    <p>PHP5</p>
+<p>PHP5</p>
+<p>5a version de PHP</p>
+<p>50</p>
+<p>5050</p>
+<p>25502500</p>
+<p>MySQL</p>
 
-        echo '<ul>';
-        echo '<li>$_myvar es válida porque inicia con guión bajo.</li>';
-        echo '<li>$_7var es válida porque inicia con guión bajo.</li>';
-        echo '<li>$myvar es válida porque inicia con una letra.</li>';
-        echo '<li>$var7 es válida porque inicia con una letra.</li>';
-        echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
-        echo '</ul>';
-    ?>
+    <h2>Ejercicio 4</h2>
+    <p>PHP5</p>
+<p>PHP5</p>
+<p>5a version de PHP</p>
+<p>50</p>
+<p>PHP55a version de PHP</p>
+<p>250</p>
+<p>MySQL</p>
+ <h2>Ejercicio 5</h2>
+<p>Valor de $a: bool(false)</p>
+<p>Valor de $b: bool(false)</p>
+<p>Valor de $c: bool(false)</p>
+<p>Valor de $d: bool(true)</p>
+<p>Valor de $e: bool(false)</p>
+<p>Valor de $f: bool(true)</p>
+<p></p>
+<p>Valor de $c: true</p>
+<p>Valor de $e: true</p>
+<h2>Ejercicio 6</h2>
+   <p>Valor de $a: 9000</p>
+<p>Valor de $b: 9000</p>
+<p>Valor de $c: 9000</p>
+
+<h2>Ejercicio 7</h2>
+    <p>Versión de Apache: Apache/2.4.54 (Win64) OpenSSL/1.1.1p PHP/7.4.33</p>
+<p>Versión de PHP: 7.4.33</p>
+<p>Nombre del sistema operativo del servidor: Windows NT PCRA 10.0 build 22621 (Windows 10) AMD64</p>
+<p>Idioma del navegador (cliente): es-ES,es;q=0.9</p>
+
+
 </body>
 </html>
